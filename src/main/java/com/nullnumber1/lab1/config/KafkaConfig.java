24a22056ca9448868768b9bd78e8256a58f7254c
Yulika19 <ulika14.02@mail.ru>
2024-05-27 16:06:32 +0300

    blps 3 lab

--- src/main/java/com/nullnumber1/lab1/config/KafkaConfig.java
+++ src/main/java/com/nullnumber1/lab1/config/KafkaConfig.java
@@ -1,7 +1,5 @@
 package com.nullnumber1.lab1.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
